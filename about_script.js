0946aa734e13d7b89fdb62d8a7970edf19f7ca26
emily-sakaguchi <122579546+emily-sakaguchi@users.noreply.github.com>
2023-04-03 14:40:42 -0400

    replaced news with flexbox

---
+++ about_script.js
@@ -0,0 +1,10 @@
+/*-------------------------------------------------------------------
+SCROLL BUTTON
+- When the button is clicked, the page will scroll down to more infomration
+--------------------------------------------------------------------*/
+
+// var button1 = document.getElementById("#btn1"); 
+
+// button1.addEventListener("click", function() {
+//     sc2.scrollIntoView();
+// });
